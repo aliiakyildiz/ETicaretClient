74b2cd705c7b57d174584f222d5e28359080c330
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-05-17 20:09:42 +0300

    Added Delete Modal Dialog

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/service/common/models/product.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
@@ -15,7 +17,8 @@ export class DeleteDirective {
     private element: ElementRef,
     private _renderer: Renderer2,
     private productService: ProductService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete-icon.png");
@@ -25,16 +28,37 @@ export class DeleteDirective {
     _renderer.appendChild(element.nativeElement, img);
   }
 
-  @Input() id:string;
-  @Output() callback: EventEmitter<any>=new EventEmitter();
+  @Input() id: string;
+  @Output() callback: EventEmitter<any> = new EventEmitter();
 
   @HostListener("click")
   async onclick() {
-    this.spinner.show(SpinnerType.BallAtom);
-    const td:HTMLTableCellElement=this.element.nativeElement;
-    await this.productService.delete(this.id);
-    $(td.parentElement).fadeOut(2000,()=>{
-      this.callback.emit();
+    this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallAtom);
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      await this.productService.delete(this.id);
+      $(td.parentElement).animate({
+        opacity: 0,
+        left: "+=50",
+        height: "toogle"
+      }, 700, () => {
+        this.callback.emit();
+      });
     });
   }
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == DeleteState.Yes)
+        afterClosed();
+    });
+  }
+}
+export enum DeleteState {
+  Yes,
+  No
 }
