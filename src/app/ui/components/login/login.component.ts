c27ecd9b5fa5eaa9c532cb87b04dceb4bca07add
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-06-19 23:04:06 +0300

    Added link check and routing

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { AuthService } from 'src/app/services/common/auth.service';
 import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
@@ -10,14 +12,24 @@ import { UserService } from 'src/app/services/common/models/user.service';
 })
 export class LoginComponent extends BaseComponent implements OnInit {
 
-  constructor(private userService: UserService, spinner: NgxSpinnerService) { 
-    super(spinner);
+  constructor(private userService: UserService, spinner: NgxSpinnerService, private authService: AuthService, private activatedRoute: ActivatedRoute, private router: Router) {
+    super(spinner)
   }
 
   ngOnInit(): void {
   }
-  async login(userNameOrEmail: string, password: string) {
-    this.showSpinner(SpinnerType.BallAtom)
-    await this.userService.login(userNameOrEmail, password,()=>this.hideSpinner(SpinnerType.BallAtom));
+
+  async login(usernameOrEmail: string, password: string) {
+    this.showSpinner(SpinnerType.BallAtom);
+    await this.userService.login(usernameOrEmail, password, () => {
+      this.authService.identityCheck();
+
+      this.activatedRoute.queryParams.subscribe(params => {
+        const returnUrl: string = params["returnUrl"];
+        if (returnUrl)
+          this.router.navigate([returnUrl]);
+      });
+      this.hideSpinner(SpinnerType.BallAtom);
+    });
   }
 }
