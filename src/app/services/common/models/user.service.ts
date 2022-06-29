299b811cddb2374536eb3e87cffe7e0aab1c42f3
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-06-30 00:36:07 +0300

    Added Google login

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,3 +1,4 @@
+import { SocialUser } from '@abacritt/angularx-social-login';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Token } from 'src/app/contracts/token/token';
@@ -39,4 +40,23 @@ export class UserService {
     callBackFunction();
   }
 
+  async googleLogin(user: SocialUser, callBackFunction?: () => void): Promise<any> {
+    const observable: Observable<SocialUser | TokenResponse> = this.httpClientService.post<SocialUser | TokenResponse>({
+      action: "google-login",
+      controller: "users"
+    }, user);
+
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("Google üzerinden giriş başarıyla sağlanmıştır.", "Giriş Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      });
+    }
+
+    callBackFunction();
+  }
 }
